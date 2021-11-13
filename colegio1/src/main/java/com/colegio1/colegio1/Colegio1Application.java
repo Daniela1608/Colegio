4733c923604b8113255584aece8556899f883090
Daniela <danielasepulveda818@gmail.com>
2021-11-13 09:16:07 -0500

    initial commit

---
+++ colegio1/src/main/java/com/colegio1/colegio1/Colegio1Application.java
@@ -0,0 +1,13 @@
+package com.colegio1.colegio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Colegio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Colegio1Application.class, args);
+	}
+
+}
---
+++ colegio1/src/test/java/com/colegio1/colegio1/Colegio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegio1.colegio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Colegio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
